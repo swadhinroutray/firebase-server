9e3b196113b4f1adfde20c1fc9aeefef6fde941a
Chakradhar <chakradharvasurama@gmail.com>
2019-12-05 20:18:49 +0530

    auth basic

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 
 const express = require('express');
+const firebase = require('firebase');
 var path = require('path');
 var site_root = path.resolve(__dirname + '/..');
 var bodyParser = require('body-parser');
@@ -12,6 +13,17 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: 'https://hotweb-43048.firebaseio.com'
 });
+firebase.initializeApp({
+    apiKey: 'AIzaSyCnZcckMPdH5Gjw6HjiKJMXifaukx2378U',
+    authDomain: 'hotweb-43048.firebaseapp.com',
+    databaseURL: 'https://hotweb-43048.firebaseio.com',
+    projectId: 'hotweb-43048',
+    storageBucket: 'hotweb-43048.appspot.com',
+    messagingSenderId: '373707440045',
+    appId: '1:373707440045:web:20c562ab23be2b43fa2b24',
+    measurementId: 'G-Q1R7XHYH3R'
+});
+firebase;
 const userRoutes = require('./routes/users');
 const forumRoutes = require('./routes/forum');
 const app = express();
--- functions/routes/users.js
+++ functions/routes/users.js
@@ -2,104 +2,141 @@
 /* eslint-disable prefer-arrow-callback */
 /* eslint-disable promise/always-return */
 const admin = require('firebase-admin'),
-    express = require("express"),
-    router = express.Router(),
-    db = admin.firestore();
+    express = require('express'),
+    firebase = require('firebase');
+(router = express.Router()), (db = admin.firestore());
 
 router.get('/home', async(req, res) => {
     try {
         await db
             .collection('users')
             .get()
-            .then(snapshot =>{
-                
-               return res.send(snapshot.docs);
+            .then(snapshot => {
+                return res.send(snapshot.docs);
             });
     } catch (err) {
         console.log(err);
-    }snapshot =>{
+    }
+    snapshot => {
         res.send(snapshot.docs);
-    }
+    };
 });
-router.get('/all',async(req,res)=>{
-    let data=[];
-    try{
-
-        if(req.query.id){
-        var userID = req.query.id;
-        try{
-            db
-            .collection('users')
-            .doc(userID)
-            .get()
-            .then(doc=>{
-                res.send(doc.data());
-            })
+router.get('/all', async(req, res) => {
+    let data = [];
+    try {
+        if (req.query.id) {
+            var userID = req.query.id;
+            try {
+                db.collection('users')
+                    .doc(userID)
+                    .get()
+                    .then(doc => {
+                        res.send(doc.data());
+                    });
+            } catch (err) {
+                console.log(err);
+            }
+        } else {
+            try {
+                await db
+                    .collection('users')
+                    .get()
+                    .then(snapshot => {
+                        snapshot.docs.forEach(doc => {
+                            data.push(doc.data());
+                        });
+                    });
+                res.send(data);
+            } catch (err) {
+                console.log('Error getting documents: ', err);
+            }
         }
-        catch(err){
-            console.log(err);
-        }
-    }
-
-    else{
-  
-      try{
-
-        await db
-        .collection("users")
-        .get()
-        .then(snapshot=> {
-        snapshot.docs.forEach((doc) => {
-            data.push(doc.data());
-        });
-    })
-    res.send(data);
-    }  
-    
-    catch(err) {
-        console.log("Error getting documents: ", err);
-        }
-    }
-    }
-    catch(err){
+    } catch (err) {
         console.log(err);
     }
-})
-        
+});
 
-router.post('/save', async(req, res)=>{
+router.post('/register', async(req, res) => {
+    try {
+        await firebase
+            .auth()
+            .createUserWithEmailAndPassword(req.body.email, req.body.password)
+            .then(() => {
+                res.send('createduser');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    } catch (err) {
+        console.log(err);
+        res.send(err);
+    }
+});
+
+router.post('/login', async(req, res) => {
+    try {
+        await firebase
+            .auth()
+            .signInWithEmailAndPassword(req.body.email, req.body.password)
+            .then(user => {
+                res.send('createduser');
+            });
+    } catch (err) {
+        res.send('wrond credentials');
+    }
+});
+
+function isLoggedIn(req, res, next) {
+    const user = firebase.auth().currentUser;
+    if (user) {
+        next();
+    } else {
+        res.send('notLoggedIn');
+    }
+}
+
+router.post('/logout', async(req, res) => {
+    try {
+        await firebase
+            .auth()
+            .signOut()
+            .then(() => {
+                res.send('signed out successfully');
+            });
+    } catch (err) {
+        res.send('failed to logout');
+    }
+});
+router.post('/save', async(req, res) => {
     var username = req.body.username;
     var email = req.body.email;
-    try{
-    await db
-    .collection('users')
-    .add({
-        name: username,
-        email:email 
-    })
-    .then(doc=>{
-       return console.log("Created:",doc.id);    // eslint-disable-next-line handle-callback-err
-    })
-}
-    catch(err){
+    try {
+        await db
+            .collection('users')
+            .add({
+                name: username,
+                email: email
+            })
+            .then(doc => {
+                return console.log('Created:', doc.id); // eslint-disable-next-line handle-callback-err
+            });
+    } catch (err) {
         console.log(err);
     }
 });
 
-router.delete('/remove',async(req,res)=>{
-    
-    try{
+router.delete('/remove', async(req, res) => {
+    try {
         var userID = req.query.id;
         await db
-        .collection('users')
-        .doc(userID)
-        .delete()
-        .then(doc=>{
-            res.send('Document Deleted!');
-        })
-    }
-    catch(err){
+            .collection('users')
+            .doc(userID)
+            .delete()
+            .then(doc => {
+                res.send('Document Deleted!');
+            });
+    } catch (err) {
         console.log(err);
     }
-})
+});
 module.exports = router;
