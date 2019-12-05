62f85dd091a69fae4490abd2d9ff8b950565312d
Chakradhar <chakradharvasurama@gmail.com>
2019-12-05 10:50:17 +0530

    get and deletes in forum

--- functions/routes/forum.js
+++ functions/routes/forum.js
@@ -3,4 +3,95 @@ const admin = require('firebase-admin'),
     router = express.Router(),
     db = admin.firestore();
 
+router.get('/all', async(req, res) => {
+    let data = [];
+    try {
+        if (req.query.id) {
+            var forumID = req.query.id;
+            try {
+                db.collection('forum')
+                    .doc(forumID)
+                    .get()
+                    .then(doc => {
+                        res.send(doc.data());
+                    });
+            } catch (err) {
+                console.log(err);
+            }
+        } else {
+            try {
+                await db
+                    .collection('forum')
+                    .get()
+                    .then(snapshot => {
+                        snapshot.docs.forEach(doc => {
+                            data.push(doc.data());
+                        });
+                    });
+                res.send(data);
+            } catch (err) {
+                console.log('Error getting documents: ', err);
+            }
+        }
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+router.delete('/remove', async(req, res) => {
+    try {
+        var forumID = req.query.id;
+        await db
+            .collection('forum')
+            .doc(forumID)
+            .delete()
+            .then(doc => {
+                res.send('Document Deleted!');
+            });
+    } catch (err) {
+        console.log(err);
+    }
+});
+router.delete('/remove', async(req, res) => {
+    try {
+        var forumID = req.query.id;
+        await db
+            .collection('forum')
+            .doc(forumID)
+            .delete()
+            .then(doc => {
+                res.send('Document Deleted!');
+            });
+    } catch (err) {
+        console.log(err);
+    }
+});
+router.delete('/removeComment', async(req, res) => {
+    try {
+        var commentID = req.query.commentid;
+        var postID = req.query.postid;
+        var newComments;
+        await db
+            .collection('forum')
+            .doc(postID)
+            .get()
+            .then(doc => {
+                data = doc.data();
+                newComments = data.comments.filter(
+                    comment => comment.commentId != commentID
+                );
+            });
+        await db
+            .collection('forum')
+            .doc(postID)
+            .update({
+                comments: newComments
+            })
+            .then(doc => {
+                res.send('Document Deleted!');
+            });
+    } catch (err) {
+        console.log(err);
+    }
+});
 module.exports = router;
